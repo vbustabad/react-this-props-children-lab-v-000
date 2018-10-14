b3209636201949df67b2aa1ea65c3351da4dcb3e
Veronica Bustabad <vbustabad@gmail.com>
2018-10-14 03:59:37 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 export default class Invitation extends React.Component {
   render() {
-    const currentPlayingTitle = 'Mad Max: Fury Road';
-    const childrenWithExtraProp = React.Children.map(this.props.children, child => {
-      return React.cloneElement(child, {
-        isPlaying: child.props.title === currentPlayingTitle
-      });
-    });
-
     return (
-      <div className="movie-browser">
-        {childrenWithExtraProp}
-      </div>
+      <h1>
+        "You've been invited!"
+      </h1>
     );
   }
 }
