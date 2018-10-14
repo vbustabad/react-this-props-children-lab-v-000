5535218b90d63ae55ef871dd7df906391658e6f7
Veronica Bustabad <vbustabad@gmail.com>
2018-10-14 03:58:05 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,16 @@
-// Code ThemedDecoration Component Here
+export default class ThemedDecorations extends React.Component {
+  render() {
+    const currentPlayingTitle = 'Mad Max: Fury Road';
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        isPlaying: child.props.title === currentPlayingTitle
+      });
+    });
+ 
+    return (
+      <div className="movie-browser">
+        {childrenWithExtraProp}
+      </div>      
+    );
+  }
+}
